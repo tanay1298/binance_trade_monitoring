045929a0f9fd517e32013a6b7b7b6cd9aa6a9643
Tanay Burreja <32588346+tanay1298@users.noreply.github.com>
2024-03-30 20:54:14 +0530

    save stream data to db

--- crypto_analytics/models.py
+++ crypto_analytics/models.py
@@ -2,5 +2,21 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Trade(models.Model):
+    event_type = models.CharField(max_length=100)
+    event_time = models.BigIntegerField()
+    symbol = models.CharField(max_length=100)
+    trade_id = models.BigIntegerField()
+    price = models.DecimalField(max_digits=20, decimal_places=8)
+    quantity = models.DecimalField(max_digits=20, decimal_places=8)
+    buy_order_id = models.BigIntegerField()
+    sell_order_id = models.BigIntegerField()
+    trade_completed_time = models.BigIntegerField()
+    is_maker = models.BooleanField()
+    is_taker = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'trade'
